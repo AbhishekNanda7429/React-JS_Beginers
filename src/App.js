f4afb59b3343729ed514602064306aa9656a6eb0
AbhishekNanda7429 <64893770+AbhishekNanda7429@users.noreply.github.com>
2022-01-14 10:40:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Component/Greet'
 
+//stateless component
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Greet />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello world!
         </p>
         <a
           className="App-link"
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/Component/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet(){
+//     return<h1> Hello world</h1>
+// }
+const Greet = () => <h1>Hello There</h1>
+
+export default Greet
